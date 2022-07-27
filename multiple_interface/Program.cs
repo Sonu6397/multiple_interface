b5ff759e9631eaed7598ee575c87b83047286524
admin <admin@CHETUIWK1860>
2022-07-27 11:43:42 +0530

    Add project files.

---
+++ multiple_interface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace multiple_interface
+{ 
+    interface test1
+    {
+        public void Mymethod();
+    }
+
+    interface test2
+    {
+        public void Show();
+      
+    }
+
+    class dog : test1, test2
+    {
+        public void Mymethod()
+        {
+            Console.WriteLine("this method is test1");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("this method is test2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dog obj = new dog();
+            obj.Mymethod();
+            obj.Show();
+            Console.ReadLine();
+        }
+    }
+}
